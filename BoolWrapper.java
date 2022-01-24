96365a9f53d643737099cc4ff59c4883c48d936f
Ashwini Aher <98300102+aherashwin@users.noreply.github.com>
2022-01-24 22:25:17 +0530

    Add files via upload

---
+++ BoolWrapper.java
@@ -0,0 +1,15 @@
+
+public class BoolWrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Boolean b=new Boolean("yes");
+				Boolean c=new Boolean("no");
+				System.out.println(b);
+				System.out.println(c);
+				System.out.println(b.equals(c));
+
+
+	}
+
+}
